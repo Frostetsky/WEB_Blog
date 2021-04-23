ba0179f9924bc378788954e020b2cd895a2f1c0f
Frostetskiy <nesterenko-nikita@bk.ru>
2021-04-24 02:05:56 +0300

    WEB-Blog | Development of the Blog section

--- src/main/java/com/webblog/WebBlog/controllers/MainController.java
+++ src/main/java/com/webblog/WebBlog/controllers/MainController.java
@@ -3,15 +3,19 @@ package com.webblog.WebBlog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("title", "Люблю Женю.");
+        model.addAttribute("title", "Главная страница");
         return "home";
     }
 
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Страница про нас.");
+        return "about";
+    }
 }
